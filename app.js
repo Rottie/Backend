47b39e5afb434732c7710488a970993ec825e19d
Rottie <kingtang2180@gmail.com>
2022-06-03 22:22:25 +0800

    first

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const res = require("express")
+const express = require("express");
+const app = express();
 
-const express =requre("express")
-const app=express()
-
-app.get("/",(req,res)){
+app.get("/", (req, res) => {
   res.json({
-    mesg:'Hellp'
-  })
-}
-
-app.listen(3000)
+    mesg: "Hellp",
+  });
+});
+// set port, listen for requests
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
